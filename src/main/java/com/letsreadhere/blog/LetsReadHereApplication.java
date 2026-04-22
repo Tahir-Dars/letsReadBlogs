f84b72376c2a00fd55143fc8d04a6072e6e94e72
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-23 03:49:54 +0500

    Initial Commit : Adding the necessary files

---
+++ src/main/java/com/letsreadhere/blog/LetsReadHereApplication.java
@@ -0,0 +1,13 @@
+package com.letsreadhere.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsReadHereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsReadHereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letsreadhere/blog/LetsReadHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsreadhere.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsReadHereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
