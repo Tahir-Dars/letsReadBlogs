1ab17ca534eb01a2990f731ce76f3cf8b47bde03
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-24 20:09:45 +0500

    Enhance User model with validation, UUID id, and timestamp

--- src/main/java/com/letsreadhere/blog/model/User.java
+++ src/main/java/com/letsreadhere/blog/model/User.java
@@ -1,12 +1,66 @@
 package com.letsreadhere.blog.model;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
 public class User {
-    private int UserID;
-    private String email;
-    private String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Pattern(regexp = "^[^0-9].{3,}$", message = "Invalid Name")
     private String name;
-//    private List<Post> postList;
+
+    @NotBlank
+    @Email(message = "Write correct email format")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$"
+
+    )
+    @Column(nullable = false)
+    private String password;
+
+    //    private List<Post> postList;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(createdAt, user.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, createdAt);
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
