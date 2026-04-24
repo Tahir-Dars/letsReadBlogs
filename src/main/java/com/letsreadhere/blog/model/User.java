fdae3c345dbff3981197132c8bd0679fdbd24961
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-24 18:29:27 +0500

    Add User model class with basic attributes

---
+++ src/main/java/com/letsreadhere/blog/model/User.java
@@ -0,0 +1,12 @@
+package com.letsreadhere.blog.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int UserID;
+    private String email;
+    private String password;
+    private String name;
+//    private List<Post> postList;
+    private LocalDateTime createdAt;
+}
