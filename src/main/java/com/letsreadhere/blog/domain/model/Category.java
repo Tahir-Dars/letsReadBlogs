d1eda779c9d4ad659788345643a92ce945d70769
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-25 05:00:13 +0500

    Add Category model with UUID id and validation constraints

---
+++ src/main/java/com/letsreadhere/blog/domain/model/Category.java
@@ -0,0 +1,42 @@
+package com.letsreadhere.blog.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "column")
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    //List<Posts> posts;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
