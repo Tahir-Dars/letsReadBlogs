cf58c90d924738a42ccdd6e31c5124c2bd57e270
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-27 21:45:48 +0500

    Add CategoryMapper interface for mapping Category entities to DTOs

---
+++ src/main/java/com/letsreadhere/blog/mapper/CategoryMapper.java
@@ -0,0 +1,29 @@
+package com.letsreadhere.blog.mapper;
+
+
+import com.letsreadhere.blog.domain.PostStatus;
+import com.letsreadhere.blog.domain.dto.CategoryDto;
+import com.letsreadhere.blog.domain.model.Category;
+import com.letsreadhere.blog.domain.model.Posts;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatedPostCount")
+    CategoryDto entityToDto(Category category);
+
+    @Named("calculatedPostCount")
+    default long calculatedPostCount(List<Posts> posts) {
+        if (posts.isEmpty()) {
+            return 0;
+        }
+        return posts.stream()
+                .filter(posts1 -> PostStatus.PUBLISHED.equals(posts1.getPostStatus()))
+                .count();
+    }
+}
