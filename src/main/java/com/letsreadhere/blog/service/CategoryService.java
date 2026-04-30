e809c7e98f1ec9006d00191a2799ff062f7fade4
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-30 15:11:27 +0500

    Add deleteCategory method to CategoryService for category removal

--- src/main/java/com/letsreadhere/blog/service/CategoryService.java
+++ src/main/java/com/letsreadhere/blog/service/CategoryService.java
@@ -3,9 +3,12 @@ package com.letsreadhere.blog.service;
 import com.letsreadhere.blog.domain.model.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
     List<Category> listAllCategories();
 
     Category createACategory(Category category);
+
+    void deleteCategory(UUID uuid);
 }
--- src/main/java/com/letsreadhere/blog/service/CategoryServiceExe.java
+++ src/main/java/com/letsreadhere/blog/service/CategoryServiceExe.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +29,17 @@ public class CategoryServiceExe implements CategoryService {
             throw new IllegalArgumentException("Category already exists with : " + category.getName());
         }
         return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(UUID uuid) {
+        Optional<Category> categoryOptional = categoryRepository.findById(uuid);
+        if (categoryOptional.isPresent()) {
+            if (categoryOptional.get().getPosts().isEmpty()) {
+                categoryRepository.deleteById(uuid);
+                return;
+            }
+        }
+        throw new IllegalStateException("Category has posts published !!");
     }
 }
