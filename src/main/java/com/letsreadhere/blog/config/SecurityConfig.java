038ca7d1524d6c80b056de514b30778bc4260bfe
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-01 16:01:31 +0500

    Addeded PasswordEncoder and AuthenticationManager beans to SecurityConfig for enhanced authentication support

--- src/main/java/com/letsreadhere/blog/config/SecurityConfig.java
+++ src/main/java/com/letsreadhere/blog/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package com.letsreadhere.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +27,15 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
