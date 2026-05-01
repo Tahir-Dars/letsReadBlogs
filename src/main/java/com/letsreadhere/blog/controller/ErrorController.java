cec8ce85a09c4e2e50e7e4138f3ad6b2aa4bd52e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-01 16:43:10 +0500

    Added BadCredentialsException import to ErrorController for enhanced error handling

--- src/main/java/com/letsreadhere/blog/controller/ErrorController.java
+++ src/main/java/com/letsreadhere/blog/controller/ErrorController.java
@@ -4,6 +4,7 @@ import com.letsreadhere.blog.domain.dto.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
