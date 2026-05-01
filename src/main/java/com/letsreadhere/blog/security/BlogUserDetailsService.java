c0bc582996d4bcd478381500216b3e089a719ef1
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-01 16:43:38 +0500

    Added BlogUserDetails and BlogUserDetailsService for user authentication

---
+++ src/main/java/com/letsreadhere/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.letsreadhere.blog.security;
+
+import com.letsreadhere.blog.domain.model.User;
+import com.letsreadhere.blog.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with Email: " + email));
+
+        return new BlogUserDetails(user);
+    }
+
+}
