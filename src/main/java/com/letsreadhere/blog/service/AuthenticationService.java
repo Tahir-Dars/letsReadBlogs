448945699fb9874200e9f2a77c88442ca9326ed6
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-02 10:56:42 +0500

    Renamed AuthenticationManager to AuthenticationService and added @Service annotation

--- src/main/java/com/letsreadhere/blog/service/AuthenticationService.java
+++ src/main/java/com/letsreadhere/blog/service/AuthenticationService.java
@@ -1,8 +1,10 @@
 package com.letsreadhere.blog.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
-public interface AuthenticationManager {
+@Service
+public interface AuthenticationService {
     UserDetails authenticateUser(String email, String password);
 
     String generateToken(UserDetails userDetails);
