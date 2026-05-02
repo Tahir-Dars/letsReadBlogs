f07baa584b5b5348b10383129bb79e8c2a265c5f
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-02 11:02:06 +0500

    Add AuthenticationController for user authentication endpoint

---
+++ src/main/java/com/letsreadhere/blog/controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.letsreadhere.blog.controller;
+
+
+import com.letsreadhere.blog.domain.dto.AuthResponse;
+import com.letsreadhere.blog.domain.dto.LoginRequest;
+import com.letsreadhere.blog.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> authResponseResponseEntity(
+            @RequestBody LoginRequest loginRequest) {
+        UserDetails userDetails = authenticationService.authenticateUser(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+        );
+        String JwtToken = authenticationService.generateToken(userDetails);
+
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(JwtToken)
+                .expiresIn(86400)
+                .build();
+
+        return ResponseEntity.ok(authResponse);
+    }
+}
