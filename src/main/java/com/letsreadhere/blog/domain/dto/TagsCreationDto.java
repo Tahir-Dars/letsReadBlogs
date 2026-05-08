0c86647512c80867f4461c4ebdbaf6db1a0bb434
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-08 17:56:17 +0500

    Added TagsCreationDto for carrying request to add tags to a post

---
+++ src/main/java/com/letsreadhere/blog/domain/dto/TagsCreationDto.java
@@ -0,0 +1,24 @@
+package com.letsreadhere.blog.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TagsCreationDto {
+    @NotBlank(message = "At least one tag name is required !")
+    @Size(max = 10, message = "Maximum {max} tags allowed")
+    private Set<
+            @Size(min = 2, max = 30, message = "Tag name must be between {min} and {max}")
+            @Pattern(regexp = "^[\\w\\s-]+$", message = "Tag name can only contain letters, numbers, spaces, and hyphens ")
+                    String> name;
+}
