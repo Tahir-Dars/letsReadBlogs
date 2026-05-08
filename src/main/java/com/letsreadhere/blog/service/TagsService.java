35b5d704a2c74d6328d5d530cf002121e7272db7
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-08 20:02:18 +0500

    Added createTag method to TagsService for batch tag creation

--- src/main/java/com/letsreadhere/blog/service/TagsService.java
+++ src/main/java/com/letsreadhere/blog/service/TagsService.java
@@ -3,7 +3,11 @@ package com.letsreadhere.blog.service;
 import com.letsreadhere.blog.domain.model.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagsService {
     List<Tag> getTags();
+
+    List<Tag> createTag(Set<String> tagNames);
+
 }
