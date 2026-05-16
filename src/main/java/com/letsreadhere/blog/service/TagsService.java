0bbc81752018759912658f9bd999aa14767f650e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-16 17:47:11 +0500

    Added deleteTag method to TagsService interface for tag removal

--- src/main/java/com/letsreadhere/blog/service/TagsService.java
+++ src/main/java/com/letsreadhere/blog/service/TagsService.java
@@ -1,13 +1,16 @@
 package com.letsreadhere.blog.service;
 
+import com.letsreadhere.blog.domain.dto.TagsResponse;
 import com.letsreadhere.blog.domain.model.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagsService {
     List<Tag> getTags();
 
     List<Tag> createTag(Set<String> tagNames);
 
+    Tag deleteTag(UUID id);
 }
