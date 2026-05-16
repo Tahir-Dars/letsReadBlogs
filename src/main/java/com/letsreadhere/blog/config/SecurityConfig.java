305e6a6ca06fcec12aee0aa536a376b1166a79e6
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-16 21:05:12 +0500

    Enforced authentication for all requests in SecurityConfig

--- src/main/java/com/letsreadhere/blog/config/SecurityConfig.java
+++ src/main/java/com/letsreadhere/blog/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
