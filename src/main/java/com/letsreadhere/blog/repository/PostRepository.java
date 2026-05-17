71e5e0c2514aa572d141d3d3df3c726a423ea43e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-17 12:31:59 +0500

    Method custom method "findAllByPostStatusAndCategoryItBelongsToAndTagsContaining" added to get every post from DB.

--- src/main/java/com/letsreadhere/blog/repository/PostRepository.java
+++ src/main/java/com/letsreadhere/blog/repository/PostRepository.java
@@ -1,11 +1,17 @@
 package com.letsreadhere.blog.repository;
 
+import com.letsreadhere.blog.domain.PostStatus;
+import com.letsreadhere.blog.domain.model.Category;
 import com.letsreadhere.blog.domain.model.Posts;
+import com.letsreadhere.blog.domain.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Posts, UUID> {
+
+    List<Posts> findAllByPostStatusAndCategoryItBelongsToAndTagsContaining(PostStatus status, Category category, Tag tag);
 }
