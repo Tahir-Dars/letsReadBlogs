eeb5e9307c9c3ba035581a6ab2195e9e1dc3064c
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-05-19 11:50:06 +0500

    Refactored the name so that it clearly defines its Identity.

--- src/main/java/com/letsreadhere/blog/domain/dto/UpdatePostRequestDto.java
+++ src/main/java/com/letsreadhere/blog/domain/dto/UpdatePostRequestDto.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UpdatePostRequest {
+public class UpdatePostRequestDto {
 
     @NotNull(message = "Post ID is required")
     private UUID uuid;
